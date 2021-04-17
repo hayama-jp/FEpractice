4d03be51ed5ccd8ce19aac79979db4d1c25696ef
hayama-jp <hayama.naka.2011@gmail.com>
2021-04-18 03:06:14 +0900

    Write function init

--- app/app.php
+++ app/app.php
@@ -6,12 +6,55 @@
     <title>P.143</title>
 
 </head>
-<?php
 
 
 
 <body>
+    <?php
+    $k = 3;
+    $n = 5;
+    $s = array(0, 0, 0, 0, 0);
+    $l = 1;
+    $x = 0;
+    $r = 0;
 
+
+    $r = init();
+    echo '<br>', $r, '<br>';
+    while ($x < 5) {
+        echo $s[$x];
+        $x++;
+    }
+    while ($r = 0) {
+        while ($x < 5) {
+            echo $s[$x];
+            $x++;
+        }
+    }
+    
+
+    function init()
+    {
+        $k = 3;
+        $n = 5;
+        $l = 0;
+        if (1 <= $k && $k <= $n) {
+            if ($l <= $n) {
+                if ($l <= $k) {
+                    $s[$l] = 1;
+                    $l++;
+                    echo $l;
+                } else {
+                    $s[$l] = 0;
+                    $l++;
+                }
+            }
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+    ?>
 </body>
 
 
